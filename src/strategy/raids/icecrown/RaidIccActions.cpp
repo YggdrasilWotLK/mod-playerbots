ddd808a4e2635746a86db247756c42b6ac01e153
Yggdrasil WoW <github@yggdrasilwow.com>
2025-03-21 17:44:11 +0100

    further conditions

--- src/strategy/raids/icecrown/RaidIccActions.cpp
+++ src/strategy/raids/icecrown/RaidIccActions.cpp
@@ -3259,7 +3259,7 @@ bool IccLichKingAddsAction::Execute(Event event)
     bool hasWinterAura = boss->HasAura(72259) || boss->HasAura(74273) || boss->HasAura(74274) || boss->HasAura(74275);
     bool hasWinter2Aura = boss->HasAura(68981) || boss->HasAura(74270) || boss->HasAura(74271) || boss->HasAura(74272);
 	
-	if (boss && !hasWinterAura && !hasWinter2Aura) // If boss is in p2, check if bot has been thrown off platform
+	if (boss && boss->GetHealthPct() < 70 && boss->GetHealthPct() > 40) && !hasWinterAura && !hasWinter2Aura) // If boss is in p2, check if bot has been thrown off platform
     {
        float dx = bot->GetPositionX() - 503.0f;
        float dy = bot->GetPositionY() - (-2124.0f);
